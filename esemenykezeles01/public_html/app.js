c9eb00891763bf63197a6ef3e8c6f0ee5a8fc879
tkramalics <tkramalics@hotmail.com>
2015-02-18 11:26:40 +0100

    esemenykezeles projekt
    2.ora

---
+++ esemenykezeles01/public_html/app.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    $("BUTTON").click():gombkattintas;
+  });
+
+function gombkattintas(e) {
+    e.target.innerHTML = "kattintas utan";
+    console.log("kattintas tortent");
+}
+
+
